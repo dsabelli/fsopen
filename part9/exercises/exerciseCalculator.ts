3d2939c4adf1d161ad5cd260bff6c5f98e142cac
dsabelli <dsabelli3@gmail.com>
2022-08-10 17:51:32 -0400

    exercise 9.2

---
+++ part9/exercises/exerciseCalculator.ts
@@ -0,0 +1,23 @@
+interface ExerciseValues {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (arr: number[], target: number): ExerciseValues => {
+  return {
+    periodLength: arr.length,
+    trainingDays: arr.filter((a) => Number(a) !== Number(0)).length,
+    success: arr.find((a) => a < target && a !== 0) ? false : true,
+    rating: 2,
+    ratingDescription: "Do better",
+    target: target,
+    average: arr.reduce((sum, current) => sum + current, 0) / arr.length,
+  };
+};
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 3], 2));
