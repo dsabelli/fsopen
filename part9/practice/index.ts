3cc0eb5af8ac4af5a116e933c0879b6418452a32
dsabelli <dsabelli3@gmail.com>
2022-08-11 14:14:01 -0400

    practice http dev server

---
+++ part9/practice/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/ping", (_req: any, res: any) => {
+  res.send("pong");
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
