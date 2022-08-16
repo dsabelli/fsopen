cdf2b5b0ee07f2e727525d48d8be50df001e5fff
dsabelli <dsabelli3@gmail.com>
2022-08-16 08:52:35 -0400

    exercises 9.10

--- part9/exercises/Patientor-Backend/src/index.ts
+++ part9/exercises/Patientor-Backend/src/index.ts
@@ -1,14 +1,14 @@
 import cors from "cors";
 import express from "express";
+import { getDiagnoses } from "./services/diagnoseService";
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
 const PORT = 3001;
 
-app.get("/api/ping", (_req: any, res: any) => {
-  console.log("someone pinged here");
-  res.json("pong");
+app.get("/api/diagnoses", (_req: any, res: any) => {
+  res.json(getDiagnoses());
 });
 
 app.get("/api/patients", (_req: any, res: any) => {
