24f50bab6de94492e5d54010f5ce215eef460353
dsabelli <dsabelli3@gmail.com>
2022-08-23 08:48:58 -0400

    export interface

--- part6/redux-essentials-app/src/app/store.ts
+++ part6/redux-essentials-app/src/app/store.ts
@@ -3,7 +3,10 @@ import cartReducer from "../features/cart/cartSlice";
 import modalReducer from "../features/modal/modalSlice";
 
 export const store = configureStore({
-  reducer: { cart: cartReducer, modal: modalReducer },
+  reducer: {
+    cart: cartReducer,
+    modal: modalReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- part6/redux-essentials-app/src/features/modal/modalSlice.ts
+++ part6/redux-essentials-app/src/features/modal/modalSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-interface isOpen {
+export interface isOpen {
   isOpen: boolean;
 }
 
