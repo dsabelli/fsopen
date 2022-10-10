aff52596667da48b0205999fcfbdbb29931185db
dsabelli <dsabelli3@gmail.com>
2022-10-10 12:11:44 -0400

    remove ts for now

--- part13/index.js
+++ part13/index.js
@@ -1,15 +1,17 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize");
+const express = require("express");
+const app = express();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    sequelize.close();
-  } catch (err) {
-    console.error("Unable to connect to the database", err);
-  }
-};
-main();
+app.get("/api/notes", async (req, res) => {
+  const notes = await sequelize.query("SELECT * FROM notes", {
+    type: QueryTypes.SELECT,
+  });
+  res.json(notes);
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
