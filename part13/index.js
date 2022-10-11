a9167b14b08c6be4851c37af7930113c1e0026c7
dsabelli <dsabelli3@gmail.com>
2022-10-11 09:50:41 -0400

    refator and add user and login

--- part13/index.js
+++ part13/index.js
@@ -1,17 +1,23 @@
-require("dotenv").config();
-const { Sequelize, QueryTypes, Model, DataTypes } = require("sequelize");
 const express = require("express");
 const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
-app.get("/api/notes", async (req, res) => {
-  const notes = await sequelize.query("SELECT * FROM notes", {
-    type: QueryTypes.SELECT,
+const notesRouter = require("./controllers/notes");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+app.use(express.json());
+
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
-  res.json(notes);
-});
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+};
+
+start();
